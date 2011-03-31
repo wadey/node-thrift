0e1b517ee5fa1d967db5ac13c1039e9f4e0a943a
TAGOMORI Satoshi <tagomoris@livedoor.jp>
2011-03-31 11:44:08 +0900

    add test script for multi transport layers

---
+++ examples/server_multitransport.js
@@ -0,0 +1,29 @@
+var thrift = require('thrift');
+
+var UserStorage = require('./gen-nodejs/UserStorage.js'),
+    ttypes = require('./gen-nodejs/user_types');
+
+var users = {};
+
+var store = function(user, success) {
+    console.log("stored:", user.uid);
+    users[user.uid] = user;
+    success();
+};
+var retrieve = function(uid, success) {
+    console.log("retrieved:", uid);
+    success(users[uid]);
+};
+
+var server_framed = thrift.createServer(UserStorage, {
+    store: store,
+    retrieve: retrieve
+});
+server_framed.listen(9090);
+/*
+var server_buffered = thrift.createServer(UserStorage, {
+    store: store,
+    retrieve: retrieve
+}, {transport: 'buffered'});
+server_buffered.listen(9091);
+*/
